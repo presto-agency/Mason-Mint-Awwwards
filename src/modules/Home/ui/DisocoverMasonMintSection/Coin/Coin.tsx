0965246bae9cf5477bb745896567075acc5cdd4d
Andriy Yurchuk <paltashka50@gmail.com>
2023-09-07 21:33:42 +0300

    remove coin initial and exit animations

--- src/modules/Home/ui/DisocoverMasonMintSection/Coin/Coin.tsx
+++ src/modules/Home/ui/DisocoverMasonMintSection/Coin/Coin.tsx
@@ -1,41 +1,36 @@
-import { FC, useEffect, useMemo, useRef, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { FlipCoinTypes } from '../assets/FlipCoinTypes'
-import {
-  MotionValue,
-  motion,
-  useMotionValueEvent,
-  useTransform,
-} from 'framer-motion'
+import { MotionValue, useMotionValueEvent, useTransform } from 'framer-motion'
 
 import Lottie, { LottieRefCurrentProps } from 'lottie-react'
 const loaderJsonPromise = import('../assets/flipCoin.json')
 
 import styles from '../DiscoverMasonMintSection.module.scss'
+import useWindowDimensions from '@/hooks/useWindowDimensions'
 
 type CoinProps = {
   scrollYProgress: MotionValue<number>
 }
 
-const initial = {
-  opacity: 0,
-  x: '-50%',
-  y: '-50%',
-}
-
-const transition = {
-  duration: 0.3,
-  ease: 'easeInOut',
-}
-
 export const Coin: FC<CoinProps> = ({ scrollYProgress }) => {
   const refLottie = useRef<LottieRefCurrentProps | null>(null)
-  const progress = useTransform(scrollYProgress, [0, 1], [0, 62])
+  const progress = useTransform(scrollYProgress, [0, 1], [0, 61])
+  const { width } = useWindowDimensions()
 
   const [loaderJson, setLoaderJson] = useState<FlipCoinTypes | null>(null)
   const [prevProgress, setPrevProgress] = useState(0)
 
   useMotionValueEvent(progress, 'change', (latest) => {
     const roundedLatest = Math.round(latest)
+
+    if (width < 767) {
+      if (roundedLatest === 0 && prevProgress > 50) return
+    }
+
+    if (roundedLatest === 0 && prevProgress > 58) {
+      return
+    }
+
     if (roundedLatest !== prevProgress) {
       refLottie.current?.goToAndStop(roundedLatest, true)
       setPrevProgress(roundedLatest)
@@ -48,27 +43,13 @@ export const Coin: FC<CoinProps> = ({ scrollYProgress }) => {
     })
   }, [])
 
-  const animate = useMemo(() => {
-    return {
-      opacity: prevProgress >= 5 && prevProgress <= 50 ? 1 : 0,
-      x: '-50%',
-      y: prevProgress >= 5 && prevProgress <= 50 ? '-50%' : '0%',
-      scaleY: prevProgress >= 5 && prevProgress <= 50 ? 1 : 0.9,
-    }
-  }, [prevProgress])
-
   return (
-    <motion.div
-      initial={initial}
-      animate={animate}
-      transition={transition}
-      className={styles['imageContainer']}
-    >
+    <div className={styles['imageContainer']}>
       <Lottie
         animationData={loaderJson}
         lottieRef={refLottie}
         autoplay={false}
       />
-    </motion.div>
+    </div>
   )
 }
