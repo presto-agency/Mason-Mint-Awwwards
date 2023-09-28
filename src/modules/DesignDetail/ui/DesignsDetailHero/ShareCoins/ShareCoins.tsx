9f82418fdf6b4e7864a1e7bfef7f11a15ffa1ece
Andriy Yurchuk <paltashka50@gmail.com>
2023-09-28 17:55:30 +0300

    add share coins functionality

--- src/modules/DesignDetail/ui/DesignsDetailHero/ShareCoins/ShareCoins.tsx
+++ src/modules/DesignDetail/ui/DesignsDetailHero/ShareCoins/ShareCoins.tsx
@@ -1,6 +1,11 @@
-import { FC, memo } from 'react'
+import { FC, memo, useCallback } from 'react'
 import classNames from 'classnames'
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import toast, { Toaster } from 'react-hot-toast'
+import { useCopyToClipboard } from 'usehooks-ts'
 import { ProductProps } from '@/utils/types'
 
 const AnimatedElement = dynamic(
@@ -20,16 +25,39 @@ type ShareCoinsProps = {
 }
 
 const ShareCoins: FC<ShareCoinsProps> = ({ className }) => {
+  const { asPath } = useRouter()
+  const [_, copy] = useCopyToClipboard()
+  const sharedUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${asPath}`
+
+  const handleCopy = useCallback(() => {
+    copy(sharedUrl).then(() => {
+      toast.success('Link copied!')
+    })
+  }, [sharedUrl, copy])
+
   return (
     <div className={classNames(styles['ShareCoins'], className)}>
+      <Toaster position="bottom-right" />
       <AnimatedElement>
         <h6>share coins</h6>
       </AnimatedElement>
       <AnimatedElement className={styles['linksList']}>
-        <LinkIcon className={styles['icon']} />
-        <Facebook className={styles['icon']} />
-        <Twitter className={styles['icon']} />
-        <Linkedin className={styles['icon']} />
+        <LinkIcon className={styles['icon']} onClick={handleCopy} />
+        <Link
+          href={`https://www.facebook.com/sharer/sharer.php?u=${sharedUrl}`}
+        >
+          <Facebook className={styles['icon']} />
+        </Link>
+        <Link
+          href={`https://twitter.com/intent/tweet?url=${sharedUrl}&via=twitterhandle`}
+        >
+          <Twitter className={styles['icon']} />
+        </Link>
+        <Link
+          href={`https://www.linkedin.com/sharing/share-offsite/?url=${sharedUrl}`}
+        >
+          <Linkedin className={styles['icon']} />
+        </Link>
       </AnimatedElement>
     </div>
   )
